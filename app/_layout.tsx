dbddcb97086293d86df8ea2ac87e349eff110b14
junichi haga <niwatori1212@gmail.com>
2024-12-22 23:51:20 +0900

    ドロワーのメニュー管理をDrawer.Screenで行う

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,10 +5,17 @@ import { View, Text } from "react-native";
 export default function Layout() {
   return (
     <Drawer
-      screenOptions={{
-        headerShown: false, // ドロワーメニューでの親ヘッダーを非表示
-      }}
+    // screenOptions={{
+    //   headerShown: false, // ドロワーメニューでの親ヘッダーを非表示
+    // }}
     >
+      <Drawer.Screen
+        name="(tabs)"
+        options={{
+          drawerLabel: "Tabs",
+          headerShown: false, // タブ内で独自のヘッダーを管理
+        }}
+      />
       <Drawer.Screen name="settings" options={{ drawerLabel: "Settings" }} />
     </Drawer>
   );
