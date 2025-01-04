9f9a8db113fbda86e583e0920428be7bfa9d9e9b
junichi haga <niwatori1212@gmail.com>
2025-01-04 19:46:26 +0900

    モーダルが画面を覆うようにする

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,11 @@
 import { Stack } from "expo-router";
+import { Pressable, Text } from "react-native";
+import { useRouter } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Layout() {
+  const router = useRouter();
+
   return (
     <Stack>
       <Stack.Screen
@@ -12,13 +17,39 @@ export default function Layout() {
       <Stack.Screen
         name="modal"
         options={{
-          presentation: "modal",
+          //presentation: "modal",
+          headerLeft: () => (
+            <Pressable onPress={() => router.back()}>
+              <Ionicons name="close" size={24} color="black" />
+            </Pressable>
+          ),
+          contentStyle: {
+            backgroundColor: "white", // 背景色を設定
+          },
+          animation: "slide_from_bottom", // 下からスライド
+          headerStyle: {
+            backgroundColor: "blue", // ヘッダーの背景色
+          },
+          presentation: "transparentModal", // 前の画面を暗く表示
         }}
       />
       <Stack.Screen
         name="setting"
         options={{
-          presentation: "modal",
+          // presentation: "modal",
+          headerLeft: () => (
+            <Pressable onPress={() => router.back()}>
+              <Ionicons name="close" size={24} color="black" />
+            </Pressable>
+          ),
+          contentStyle: {
+            backgroundColor: "white",
+          },
+          animation: "slide_from_bottom",
+          headerStyle: {
+            backgroundColor: "blue",
+          },
+          presentation: "transparentModal",
         }}
       />
     </Stack>
