7c9060457fbc9d4eca2bc34b917faa9f3f526903
junichi haga <niwatori1212@gmail.com>
2025-01-05 21:46:05 +0900

    MaterialTopTabsの正常動作

--- app/(main)/(tabs)/(2list)/_layout.tsx
+++ app/(main)/(tabs)/(2list)/_layout.tsx
@@ -1,8 +1,32 @@
-import { Stack } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import { Stack, useRouter } from "expo-router";
+import { Pressable } from "react-native";
+import { DrawerNavigationProp } from "@react-navigation/drawer";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Layout() {
+  const router = useRouter();
+  const navigation = useNavigation<DrawerNavigationProp<{}>>();
+
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerLeft: ({ canGoBack }) =>
+          canGoBack ? (
+            // 戻れる画面がある場合、戻るボタンを表示
+            <Pressable onPress={() => router.back()}>
+              <Ionicons name="arrow-back" size={24} color="black" />
+            </Pressable>
+          ) : (
+            // 戻れる画面がない場合、ハンバーガーメニューを表示
+            <Pressable onPress={() => navigation.openDrawer()}>
+              <Ionicons name="menu" size={24} color="black" />
+            </Pressable>
+          ),
+        headerShown: true,
+        headerTitleAlign: "center", // タイトルを中央揃え
+      }}
+    >
       <Stack.Screen name="(top-tabs)" options={{ title: "Top Tabs" }} />
     </Stack>
   );
