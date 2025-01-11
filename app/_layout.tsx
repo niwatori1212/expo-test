66cca5c96a4a1af6c86871e1658b323dc082411b
junichi haga <niwatori1212@gmail.com>
2025-01-11 18:31:31 +0900

    tamaguiの導入

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,56 +2,60 @@ import { Stack } from "expo-router";
 import { Pressable } from "react-native";
 import { useRouter } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
+import { TamaguiProvider } from "tamagui";
+import { tamaguiConfig } from "../tamagui.config";
 
 export default function Layout() {
   const router = useRouter();
 
   return (
-    <Stack>
-      <Stack.Screen
-        name="(main)"
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="modal"
-        options={{
-          //presentation: "modal",
-          headerLeft: () => (
-            <Pressable onPress={() => router.back()}>
-              <Ionicons name="close" size={24} color="black" />
-            </Pressable>
-          ),
-          contentStyle: {
-            backgroundColor: "white", // 背景色を設定
-          },
-          animation: "slide_from_bottom", // 下からスライド
-          headerStyle: {
-            backgroundColor: "blue", // ヘッダーの背景色
-          },
-          presentation: "transparentModal", // 前の画面を暗く表示
-        }}
-      />
-      <Stack.Screen
-        name="setting"
-        options={{
-          // presentation: "modal",
-          headerLeft: () => (
-            <Pressable onPress={() => router.back()}>
-              <Ionicons name="close" size={24} color="black" />
-            </Pressable>
-          ),
-          contentStyle: {
-            backgroundColor: "white",
-          },
-          animation: "slide_from_bottom",
-          headerStyle: {
-            backgroundColor: "blue",
-          },
-          presentation: "transparentModal",
-        }}
-      />
-    </Stack>
+    <TamaguiProvider config={tamaguiConfig}>
+      <Stack>
+        <Stack.Screen
+          name="(main)"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="modal"
+          options={{
+            //presentation: "modal",
+            headerLeft: () => (
+              <Pressable onPress={() => router.back()}>
+                <Ionicons name="close" size={24} color="black" />
+              </Pressable>
+            ),
+            contentStyle: {
+              backgroundColor: "white", // 背景色を設定
+            },
+            animation: "slide_from_bottom", // 下からスライド
+            headerStyle: {
+              backgroundColor: "blue", // ヘッダーの背景色
+            },
+            presentation: "transparentModal", // 前の画面を暗く表示
+          }}
+        />
+        <Stack.Screen
+          name="setting"
+          options={{
+            // presentation: "modal",
+            headerLeft: () => (
+              <Pressable onPress={() => router.back()}>
+                <Ionicons name="close" size={24} color="black" />
+              </Pressable>
+            ),
+            contentStyle: {
+              backgroundColor: "white",
+            },
+            animation: "slide_from_bottom",
+            headerStyle: {
+              backgroundColor: "blue",
+            },
+            presentation: "transparentModal",
+          }}
+        />
+      </Stack>
+    </TamaguiProvider>
   );
 }
